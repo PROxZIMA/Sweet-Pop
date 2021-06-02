df0c9e9c4a117ff643c6685cbb35655972004096
PROxZIMA <pratikbpingale9075@gmail.com>
2021-06-02 14:31:56 +0530

    Auto user preference

---
+++ programs/user.js
@@ -0,0 +1,5 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("browser.tabs.tabMinWidth", 76);
+user_pref("browser.compactmode.show", true);
+user_pref("browser.proton.enabled", false);
