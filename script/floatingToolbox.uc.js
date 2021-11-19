1394b68ece39de3be6c9948c7cd0340990d2bb45
PROxZIMA <pratikbpingale9075@gmail.com>
2021-11-19 17:31:15 +0530

    Add: Floating Toolbox userJS

---
+++ script/floatingToolbox.uc.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name           Floating Toolbox
+// @version        0.0.1
+// @author         PROxZIMA
+// @homepage       https://github.com/PROxZIMA
+// @note           specifically designed for https://github.com/PROxZIMA/Sweet-Pop
+// @note           fx-autoconfig https://github.com/MrOtherGuy/fx-autoconfig
+// @description    Enable/disable floating Toolbox using `about:config`:`userChromeJS.floatingToolbox.enabled` or from `Toolbox Menu > Tools > userscripts > Floating Toolbox`
+// ==/UserScript==
+
+(function () {
+    var prefs = Services.prefs,
+        enabled;
+    if (prefs.prefHasUserValue('userChromeJS.floatingToolbox.enabled')) {
+        enabled = prefs.getBoolPref('userChromeJS.floatingToolbox.enabled')
+    } else {
+        prefs.setBoolPref('userChromeJS.floatingToolbox.enabled', true);
+        enabled = true;
+    }
+
+    var css = `
+    #main-window:not([customizing="true"]) #navigator-toolbox {
+        display: grid;
+        transition: transform 85ms linear, opacity 85ms linear !important;
+        transform-origin: top;
+        transform: rotateX(75deg);
+        opacity: 0;
+    }
+    `;
+    
+    var sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
+    var uri = makeURI('data:text/css;charset=UTF=8,' + encodeURIComponent(css));
+
+    if (enabled) {
+        sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+    }
+})();
