c5414394bdd32b915cd1ffda93a3e7ea4fe41025
PROxZIMA <pratikbpingale9075@gmail.com>
2022-01-26 21:23:41 +0530

    Refactor: Removed uidensity pref

--- programs/user.js
+++ programs/user.js
@@ -3,5 +3,5 @@ user_pref("svg.context-properties.content.enabled", true);
 user_pref("browser.tabs.tabMinWidth", 76);
 user_pref("browser.compactmode.show", true);
 user_pref("browser.proton.enabled", false);
-user_pref("browser.uidensity", 1);
+// user_pref("browser.uidensity", 1);
 user_pref("userChrome.toolbarSlider.width", 2);
