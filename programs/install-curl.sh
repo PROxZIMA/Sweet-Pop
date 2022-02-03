769f2e77330f596596e2f9f457586038531be33f
PROxZIMA <pratikbpingale9075@gmail.com>
2022-02-03 12:08:53 +0530

    Fix: Curl Installation script

--- programs/install-curl.sh
+++ programs/install-curl.sh
@@ -1,22 +1,19 @@
 #! /usr/bin/env bash
 
 VERSION=$(curl -s "https://github.com/PROxZIMA/Sweet-Pop/releases/latest/download" 2>&1 | sed "s/^.*download\/\([^\"]*\).*/\1/")
-FILENAME=Sweet-Pop-$VERSION.tar.gz
-FOLDERPATH=$PWD/Sweet-Pop
+FILENAME=Sweet-Pop-$VERSION.zip
+FOLDERPATH=$PWD/Sweet-Pop-master
 
 if [ -d "$FOLDERPATH" ]; then rm -Rf $FOLDERPATH; fi
 
-mkdir $FOLDERPATH
-
-cd $FOLDERPATH
-
 curl -LJo $FILENAME https://github.com/PROxZIMA/Sweet-Pop/archive/refs/heads/master.zip
 
-tar -xzf $FILENAME --strip-components=1
+unzip $FILENAME
+
 rm $FILENAME
 
-chmod +x programs/install.sh
+chmod +x "${FOLDERPATH}/programs/install.sh"
 
-./programs/install.sh
+"${FOLDERPATH}/programs/install.sh" -e
 
 if [ -d "$FOLDERPATH" ]; then rm -Rf $FOLDERPATH; fi
