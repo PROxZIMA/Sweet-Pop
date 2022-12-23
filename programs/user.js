9569be2dd065eafdcdee9a67c18039eda9bca4d5
PROxZIMA <pratikbpingale9075@gmail.com>
2022-12-23 12:37:01 +0530

    Add: Enable experimental has selector

--- programs/user.js
+++ programs/user.js
@@ -5,3 +5,4 @@ user_pref("browser.compactmode.show", true);
 user_pref("browser.proton.enabled", false);
 // user_pref("browser.uidensity", 1);
 user_pref("userChrome.toolbarSlider.width", 2);
+user_pref("layout.css.has-selector.enabled", true);
